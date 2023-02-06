4e621ed79adb985474806b8c321f263a8df65854
Kang Kujun <donato14@naver.com>
2023-02-06 22:57:06 +0900

    ✨ add v-if, modal

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
 
+    <div class="black-bg" v-if="모달창상태 === true" @click="모달창상태 = false">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지 내용</p>
+      </div>
+    </div>
+
     <div class="menu">
       <a href="" v-for="(작명,i) in menu" :key="i">{{작명}}</a>
     </div>
@@ -11,16 +18,19 @@
       <p>50 만원</p>
     </div> -->
     <div>
-      <h4>{{products[0]}}</h4>
+      <img src="./assets/room0.jpg" class="room-img">
+      <h4 @click="모달창상태 = true">{{products[0]}}</h4>
       <p>50만원</p>
       <button @click="++신고수[0]">허위매물 신고</button> <span>신고수 : {{신고수[0]}}</span>
     </div>
     <div>
+      <img src="./assets/room1.jpg" class="room-img">
       <h4>{{products[1]}}</h4>
       <p>50만원</p>
       <button @click="++신고수[1]">허위매물 신고</button> <span>신고수 : {{신고수[1]}}</span>
     </div>
     <div>
+      <img src="./assets/room2.jpg" class="room-img">
       <h4>{{products[2]}}</h4>
       <p>51만원</p>
       <button @click="++신고수[2]">허위매물 신고</button> <span>신고수 : {{신고수[2]}}</span>
@@ -36,6 +46,7 @@ export default {
   name: 'App',
   data(){
     return {
+      모달창상태 : false,
       신고수 : [0,0,0],
       products: ['역삼동원룸','천호동원룸','마포구원룸'],
       menu: ['Home', 'Shop', 'About']
@@ -72,5 +83,29 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
