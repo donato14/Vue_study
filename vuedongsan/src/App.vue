f124c886c2a5823184adc6ecf24305e0704fa0d9
Kang Kujun <donato14@naver.com>
2023-02-08 21:50:31 +0900

    ⚡️ 모달창 상세페이지

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -3,8 +3,10 @@
 
     <div class="black-bg" v-if="모달창상태 === true">
       <div class="white-bg">
-        <h4>상세페이지</h4>
-        <p>상세페이지 내용</p>
+        <img :src="원룸들[누른번호].image" class="room-img">
+        <h4>{{ 원룸들[누른번호].title }}</h4>
+        <p>{{ 원룸들[누른번호].content}}</p>
+        <p>{{ 원룸들[누른번호].price }}</p>
         <button @click="모달창상태 = false">닫기</button>
       </div>
     </div>
@@ -16,27 +18,10 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <div v-for="(작명2,i) in 원룸들" :key="i">
       <img :src="작명2.image" class="room-img">
-      <h4>{{ 작명2.title }}</h4>
+      <h4 @click="모달창상태 = true; 누른번호 = i">{{ 작명2.title }}</h4>
       <p>{{작명2.price}} 원</p>
     </div>
-    <!-- <div>
-      <img src="./assets/room0.jpg" class="room-img">
-      <h4 @click="모달창상태 = true">{{products[0]}}</h4>
-      <p>50만원</p>
-      <button @click="++신고수[0]">허위매물 신고</button> <span>신고수 : {{신고수[0]}}</span>
-    </div>
-    <div>
-      <img src="./assets/room1.jpg" class="room-img">
-      <h4>{{products[1]}}</h4>
-      <p>50만원</p>
-      <button @click="++신고수[1]">허위매물 신고</button> <span>신고수 : {{신고수[1]}}</span>
-    </div>
-    <div>
-      <img src="./assets/room2.jpg" class="room-img">
-      <h4>{{products[2]}}</h4>
-      <p>51만원</p>
-      <button @click="++신고수[2]">허위매물 신고</button> <span>신고수 : {{신고수[2]}}</span>
-    </div> -->
+
     
   </div>
 </template>
@@ -51,6 +36,7 @@ export default {
   data(){
     return {
       모달창상태 : false,
+      누른번호: 0,
       신고수 : [0,0,0],
       menu: ['Home', 'Shop', 'About'],
       products: ['역삼동원룸','천호동원룸','마포구원룸'],
