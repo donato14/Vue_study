c31421786eb4c3a1a6359787b3fba724e6ad978e
Kang Kujun <donato14@naver.com>
2023-02-09 22:31:24 +0900

    v-model

--- vuedongsan/src/Modal.vue
+++ vuedongsan/src/Modal.vue
@@ -4,7 +4,9 @@
       <img :src="원룸들[누른번호].image" class="room-img">
       <h4>{{ 원룸들[누른번호].title }}</h4>
       <p>{{ 원룸들[누른번호].content}}</p>
-      <p>{{ 원룸들[누른번호].price }}</p>
+      <!-- <input type="text" @input="month = $event.target.value"> -->
+      <input type="text" v-model="month">
+      <p> {{month}}개월 선택함 : {{ 원룸들[누른번호].price*month }} 원</p>
       <DiscountBanner/>
       <button @click="$emit('closeModal')">닫기</button>
     </div>
@@ -14,6 +16,11 @@
 <script>
 export default {
   name: 'SelectedModal',
+  data(){
+    return{
+      month : 1,
+    }
+  },
   props: {
     원룸들 : Array,
     누른번호 : Number,
