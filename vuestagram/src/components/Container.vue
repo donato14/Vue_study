32f2e82af6cfcc570d5819f2d7057f188dc1cf12
Kang Kujun <donato14@naver.com>
2023-02-15 22:41:23 +0900

    데이터 바인딩

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <post :게시물="게시물[i]" v-for="(a,i) in 게시물" :key="i"/>
+  </div>
+</template>
+
+<script>
+import post from './Post.vue'
+
+export default {
+  components: { post:post },
+  name: 'ContainerContent',
+  props: {
+    게시물 : Array,
+  },
+}
+</script>
+
+<style>
+
+</style>
